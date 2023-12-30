a708aafda6affa6967ef75d383a2ae0a2e7b74cb
thiago <thiagotsdelima@gmail.com>
2023-12-30 16:25:19 -0300

    putting it online

--- src/configs/auth.js
+++ src/configs/auth.js
@@ -3,7 +3,7 @@
 module.exports = {
 
   jwt: {
-    secret: "default",
+    secret: process.env.AUTH_SECRET || "default",
     expiresIn: "1d"
   }
 }
--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 
 
-
-require('express-async-errors')
+require('express-async-errors');
+require("dotenv/config");
 
 const migrationsRun = require('./database/sqlite/migrations')
 
@@ -49,6 +49,6 @@ app.use((error, request, response, next) => {
 })
 
 
-const PORT = 3333
+const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
